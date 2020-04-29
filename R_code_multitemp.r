8974ac86b8587deccb0dcd16cdbdfaa4b09d8db1
EdoardoSantinelli <62288933+EdoardoSantinelli@users.noreply.github.com>
2020-04-29 13:07:21 +0200

    Update R_code_multitemp.r

--- R_code_multitemp.r
+++ R_code_multitemp.r
@@ -1,3 +1,70 @@
 # R code analisi multitemporale di variazione della land cover
 
+setwd("/Users/edoardosantinelli/Desktop/lab")
+library(raster)
 
+defor1 <- brick("defor1.png")
+defor2 <- brick("defor2.png")
+
+# names: defor1_.1 = NIR ,  defof1_.2= red , defor1_.3= green
+
+plotRGB(defor1,r=1, g=2, b=3, stretch="Lin")
+
+# Excercise plot seconda data
+
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+par(mfrow=c(2,1))
+plotRGB(defor1,r=1, g=2, b=3, stretch="Lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+# classificazione non supervisionata (non diamo input al pc su quali zone sono foresta o meno)
+library(RStoolbox)
+
+
+d1c <- unsuperClass(defor1, nClasses=2)
+
+plot(d1c$map)
+cl <- colorRampPalette(c('black','green'))(100) # 
+plot(d1c$map, col=cl)
+
+# classificazione di defor2
+#excercise : classificare con due classsi l'immagine defor2
+d2c <- unsuperClass(defor2, nClasses=2)
+plot(d2c$map, col=cl)
+dev.off()
+
+# plot delle 2 mappe ottenute
+
+par(mfrow=c(2,1))
+plot(d1c$map, col=cl)
+plot(d2c$map, col=cl)
+
+freq(d1c$map)
+# aree aperte =35213
+# foresta = 306079
+
+totd1 <- 306079 + 35213
+totd1
+
+percent1 <- freq(d1c$map) * 100 / 341292
+percent1
+
+# percentuali 
+# foreste = 89,6
+# aree aperte=10,4
+
+freq(d2c$map)
+totd2 <- 163094 + 179632
+percent2 <- freq(d2c$map) * 100 / totd2
+percent2
+# percentuali
+# foreste 52,42
+# aree aperte 47,58
+
+cover <- c("Agriculture","Forest")
+before <- c(10.4, 89.6)
+after <- c(47.58, 52.42)
+
+output <- data.frame(cover, before, after)
+library(ggplot2)
